6d861f6f46483d16732c371eea2758582c7ed2b2
Valtyr Farshield <valtyr.farshield@gmail.com>
2016-09-27 18:57:45 +0300

    Improve roll command

--- app/stuff/stuff.py
+++ app/stuff/stuff.py
@@ -3,29 +3,46 @@
 
 import random
 import requests
+import re
 import app.utils.upsidedown.upsidedown as upsidedown
 from . import stuff_mod
+
+ROLL_REP_MAX = 16
+ROLL_INTERVAL = 10000
 
 
 @stuff_mod.register_cmd(r'^!flip')
 @stuff_mod.register_cmd(r'^!(coin)?toss')
-def cointoss(_):
+def cointoss(data):
     coin = "HEADS" if random.randint(1, 100) <= 50 else "TAILS"
-    return "The coin landed on: `{}`".format(coin)
+    return "<@{}>, the coin landed on: `{}`".format(data['user'], coin)
 
 
 @stuff_mod.register_cmd(r'^!roll')
 def roll(data):
     cmd = data['text'].split()
     if len(cmd) == 2:
+        rolls = []
+
         try:
-            result = random.randrange(int(cmd[1]))
+            result = re.match(r'^([0-9]+)d([0-9]+)$', cmd[1], re.IGNORECASE)
+            reps = int(result.group(1)) if result else 1
+            max_roll = int(result.group(2)) if result else int(cmd[1])
+
+            if 1 <= reps <= ROLL_REP_MAX and max_roll <= ROLL_INTERVAL:
+                for _ in xrange(reps):
+                    rolls.append(random.randint(1, max_roll))
         except ValueError:
-            return "2nd parameter must be an integer greater than 0"
-        else:
-            return "You roll: `{}`".format(result)
-    else:
-        return "Proper usage is: `!roll <number>`"
+            rolls = []
+
+        if rolls:
+            if len(rolls) > 1:
+                return '<@{}>, you roll: `{} = {}`'.format(data['user'], ' + '.join(str(x) for x in rolls), sum(rolls))
+            else:
+                return '<@{}>, you roll: `{}`'.format(data['user'], rolls[0])
+
+    return "Usage: `!roll X` or `!roll AdX`, where `A` and `X` are integers greater than `0`" \
+           " and `A <= {}`, `X <= {}`".format(ROLL_REP_MAX, ROLL_INTERVAL)
 
 
 @stuff_mod.register_cmd(r'^!tableflip')
