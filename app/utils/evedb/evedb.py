b023fb1bfab433698ebbcad33ce113903bae9f34
Valtyr Farshield <valtyr.farshield@gmail.com>
2016-10-10 21:23:19 +0300

    Only search for items with an existing marketGroupID

--- app/utils/evedb/evedb.py
+++ app/utils/evedb/evedb.py
@@ -13,17 +13,18 @@ class EveDb:
             self.conn = None
 
     def get_type_id(self, name):
-        print name
         c = self.conn.cursor()
-        c.execute("select typeName, typeID from invTypes where typeName = '{0}%' collate nocase;".format(name))
+        c.execute("select typeName, typeID from invTypes "
+                  "where marketGroupID is not null and typeName = '{0}%' collate nocase;".format(name))
         result = c.fetchone()
         if result:
             return result
-        c.execute("select typeName, typeID from invTypes where typeName like '%{0}%' collate nocase;".format(name))
+        c.execute("select typeName, typeID from invTypes "
+                  "where marketGroupID is not null and typeName like '%{0}%' collate nocase;".format(name))
         results = c.fetchall()
-        if len(results)==0:
+        if len(results) == 0:
             return None
-        results = sorted(results, key=lambda x:len(x[0]))
+        results = sorted(results, key=lambda x: len(x[0]))
         return results[0]
 
 
